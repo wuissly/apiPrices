d3e06e0478725f53030a19f66940ea6fe528fbf4
wuissly <55366240+wuissly@users.noreply.github.com>
2024-01-08 11:17:50 -0300

    initial commit
    initial project setup

---
+++ src/main/java/api/challenge/apiPrices/ApiPricesApplication.java
@@ -0,0 +1,13 @@
+package api.challenge.apiPrices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPricesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/challenge/apiPrices/ApiPricesApplicationTests.java
@@ -0,0 +1,13 @@
+package api.challenge.apiPrices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
